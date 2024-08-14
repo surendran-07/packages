d5135cf37a602bb5fd5e453b048c53a258984145
surendran <162984948+surendran-07@users.noreply.github.com>
2024-08-14 11:28:39 +0530

    Add files via upload

---
+++ packages.java
@@ -0,0 +1,14 @@
+package surendran01;
+
+public class prg6 {
+	public void m1() {
+		System.out.println("m1 of prg6");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+prg6 obj=new prg6();
+obj.m1();
+	}
+
+}
